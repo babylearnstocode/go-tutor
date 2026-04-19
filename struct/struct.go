002c9cf656d4758a0dd6204bb577289c71d36e96
Huy Anh Dang <huyanh2504@gmail.com>
2026-04-19 23:38:02 +0200

    add struct

---
+++ struct/struct.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+
+
+
+type gasEngine struct{
+	mpg uint8
+	gallons uint8
+}
+
+type electricEngine struct{
+	mpkwh uint8
+	kwh uint8
+}
+
+func (e gasEngine) milesLeft() uint8{
+	return e.mpg*e.gallons;
+}
+
+func (e electricEngine) milesLeft() uint8{
+	return e.mpkwh*e.kwh;
+}
+
+type engine interface{
+	milesLeft() uint8
+}
+
+func canMakeIt(e engine, miles uint8){
+	if miles < e.milesLeft(){
+		fmt.Println("You can make it there!")
+	}else{
+		fmt.Println("need to fuel up first!")
+	}
+}
+
+func main(){
+	var myEngine gasEngine = gasEngine{25,15}
+	canMakeIt(myEngine, 200)
+}
